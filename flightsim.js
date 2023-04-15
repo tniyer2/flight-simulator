085dcf35d8ed338fad89934686a3e4b6652150e4
Tanishq Iyer <tanishqiyer88@gmail.com>
2023-04-15 01:13:51 -0400

    a

--- flightsim.js
+++ flightsim.js
@@ -441,7 +441,7 @@ function updateDroneTransform() {
                         terrainAABB.min[0] > droneAABB.min[0] ||
                         terrainAABB.min[2] > droneAABB.min[2];
 
-    if (!collision) {
+    if (!collision && !outOfBounds) {
         // if no collisions, then move the drone
         const t = gl.drone.localTransform;
         mat4.multiply(t, t, updated);
